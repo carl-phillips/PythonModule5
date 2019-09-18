eec985ca91df2bbffd4e8eb04f2c1421a098cfa4
cfphillips2 <36976465+cfphillips2@users.noreply.github.com>
2019-09-17 19:19:01 -0500

    Creating average function. Fixing average test

--- venv/input_loops/average_input_scores.py
+++ venv/input_loops/average_input_scores.py
@@ -1,2 +1,18 @@
 def average(score_list):
-    pass
+    sum = 0
+    list_length = len(score_list)
+    for scores in score_list:
+        sum += scores
+
+    avg = sum / list_length
+    return avg
+
+
+if __name__ == '__main__':
+    fname = input("Enter your first name: ")
+    lname = input("Enter your last name: ")
+    age = input("Enter your age: ")
+    score1 = input("Enter score 1: ")
+    score2 = input("Enter score 2: ")
+    score3 = input("Enter score 3: ")
+    print(fname + " " + lname + " Your average score is " + str(average([int(score1), int(score2), int(score3)])))
